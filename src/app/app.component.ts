961751a2db0ede48e1ab6f7abafb6e4d25bd4218
maheshkumarsingh <sm.singhmahesh@gmail.com>
2025-06-16 02:32:48 +0530

    mkcert and localhost https

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -7,6 +8,15 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'Client';
+export class AppComponent implements OnInit {
+  users: any[] = [];
+  title = 'MeetYourMatch';
+  httpclient = inject(HttpClient);
+  ngOnInit(): void {
+    this.httpclient.get<any[]>('https://localhost:5001/api/users').subscribe({
+      next: response => this.users = response,
+      error: error => console.error('Error fetching users:', error),
+      complete: () => console.log('User data fetch complete')
+    });
+  }
 }
