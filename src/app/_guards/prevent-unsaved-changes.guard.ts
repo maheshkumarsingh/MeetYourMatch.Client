72e576b4c78a911838541ed0e046a6ff8f58b88c
maheshkumarsingh <sm.singhmahesh@gmail.com>
2025-06-29 14:37:32 +0530

    unit of work pattern completed

--- src/app/_guards/prevent-unsaved-changes.guard.ts
+++ src/app/_guards/prevent-unsaved-changes.guard.ts
@@ -1,10 +1,19 @@
 import { CanDeactivateFn } from '@angular/router';
 import { MemberEditComponent } from '../members/member-edit/member-edit.component';
+import { inject } from '@angular/core';
+import { ConfirmService } from '../_services/confirm.service';
 
 export const preventUnsavedChangesGuard: CanDeactivateFn<MemberEditComponent> =
   (component, currentRoute, currentState, nextState) => {
+    const confirmService = inject(ConfirmService);
+
     if (component.editForm?.dirty) {
-      return confirm('You have unsaved changes. Do you really want to leave?');
+      return confirmService.confirm(
+        'Confirmation',
+        'Are you sure you want to continue? Any unsaved changes will be lost.',
+        'Yes',
+        'No'
+      ) ?? false;
     }
     return true;
   };
--- src/app/members/member-messages/member-messages.component.ts
+++ src/app/members/member-messages/member-messages.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, input, OnInit, output, ViewChild } from '@angular/core';
+import { AfterViewChecked, Component, inject, input, OnInit, output, ViewChild } from '@angular/core';
 import { Message } from '../../_models/message';
 import { MessagesService } from '../../_services/messages.service';
 import { ToastrService } from 'ngx-toastr';
@@ -11,18 +11,30 @@ import { FormsModule, NgForm } from '@angular/forms';
   templateUrl: './member-messages.component.html',
   styleUrl: './member-messages.component.css'
 })
-export class MemberMessagesComponent{
+export class MemberMessagesComponent implements AfterViewChecked{
+
   @ViewChild('messageForm') messageForm?: NgForm;
+  @ViewChild('scrollMe') scrollContainer?: any;
   messagesService = inject(MessagesService);
   userName = input.required<string>();
   //messages = input.required<Message[]>();
   toastr = inject(ToastrService);
   messageContent:string ='';
-//updateMessages = output<Message>();
+  //updateMessages = output<Message>();
+  
+  ngAfterViewChecked(): void {
+    this.scrollToBottom();
+  }
 
+  private scrollToBottom(){
+    if(this.scrollContainer){
+      this.scrollContainer.nativeElement.scrollTop = this.scrollContainer.nativeElement.scrollHeight;
+    }
+  }
   sendMessage(){
     this.messagesService.sendMessage(this.userName(), this.messageContent).then(()=>{
       this.messageForm?.reset();
+      this.scrollToBottom();
     })
     // this.messagesService.sendMessage(this.userName(), this.messageContent).subscribe({
     //   next:message=>{
